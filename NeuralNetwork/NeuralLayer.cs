93bee4b121952a8c95cd8e24777cd7228954b239
TaroBill <bill890914@gmail.com>
2021-11-03 16:18:43 +0800

    新增一些計算

--- NeuralNetwork/NeuralLayer.cs
+++ NeuralNetwork/NeuralLayer.cs
@@ -26,8 +26,46 @@ namespace NeuralNetwork
             _learningRate = learningRate;
             _bias = bias;
             _biasWeight = biasWeight;
-            for (int index = 0; index < nodesAmount; index++)
+            InitializeNodes(nodesAmount);
+        }
+
+        //初始化節點數
+        public void InitializeNodes(int amount)
+        {
+            _nodes.Clear();
+            for (int index = 0; index < amount; index++)
                 _nodes.Add(new NeuralNode());
+        }
+
+        //加入一個節點到Mode
+        public void AddNode()
+        {
+            _nodes.Add(new NeuralNode());
+        }
+
+        //計算整層的FeedForward
+        public void CalculateFeedForward(NeuralLayer inputLayer)
+        {
+            for (int nodeIndex = 0; nodeIndex < _nodes.Count(); nodeIndex++)
+                _nodes[nodeIndex].LogisticFunction(inputLayer.Nodes, nodeIndex, _bias, _biasWeight);
+        }
+
+        //計算整層的FeedForward
+        public void CalculateFeedForward(List<int> inputs)
+        {
+            if (inputs.Count() != _nodes.Count())
+                throw new Exception("輸入層與輸入的值個數不同");
+            for (int nodeIndex = 0; nodeIndex < _nodes.Count(); nodeIndex++)
+                _nodes[nodeIndex].Output = inputs[nodeIndex];
+        }
+
+
+        public List<NeuralNode> Nodes
+        {
+            get
+            {
+                return _nodes;
+            }
         }
     }
 }
